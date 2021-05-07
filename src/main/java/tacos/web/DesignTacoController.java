842c3970d667ebb8c27ee87ee9e34c0927ae0f12
Rafael Sousa Herves <rsherves@gmail.com>
2021-05-07 17:55:58 +0800

    Add Post mapping to process Taco design

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
@@ -44,5 +45,12 @@ public class DesignTacoController {
         .stream()
         .filter(x -> x.type().equals(type))
         .toList();
+  }
+
+  @PostMapping
+  public String processDesign(Taco design) {
+    // TODO save the taco design... coming in chapter 3
+    log.info("Processing Taco design: " + design);
+    return "redirect:/orders/current";
   }
 }
