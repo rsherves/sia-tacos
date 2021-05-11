883a1a6ac13c351203e85af1bb12c235589190d9
Rafael Sousa Herves <rsherves@gmail.com>
2021-05-11 11:41:46 +0800

    Declare validation rules for Taco and Order

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -3,12 +3,20 @@ package tacos;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Getter
 @Setter
 @ToString
 public class Taco {
+
+  @NotNull
+  @Size(min=5, message = "Name must be at least 5 characters long")
   private String name;
+
+  @Size(min=1, message = "You must choose at least one ingredient")
   private List<String> ingredients;
 }
