8f4380531896e69d1e5f7d9a7348de0520e5e718
Rafael Sousa Herves <rsherves@gmail.com>
2021-05-26 15:21:07 +0800

    Add Design and Order Controller tests

---
+++ src/test/java/tacos/web/OrderControllerTest.java
@@ -0,0 +1,27 @@
+package tacos.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(OrderController.class)
+class OrderControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void testCurrentOrderEndPoint() throws Exception {
+    mockMvc.perform(get("/orders/current"))
+        .andExpect(status().isOk())
+        .andExpect(view().name("orderForm"))
+        .andExpect(content().string(containsString("Order your taco creations!")));
+  }
+}
