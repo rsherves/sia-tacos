20f9a8492f054aa3eb1d6213c01a051d797170dd
Rafael Sousa Herves <rsherves@gmail.com>
2021-05-28 19:59:47 +0800

    Add fields to Taco and Order

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -7,17 +7,22 @@ import lombok.ToString;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.Collections;
 import java.util.List;
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @ToString
 public class Taco {
 
+  private long id;
+  private LocalDateTime createdAt;
+
   @NotNull
   @Size(min=5, message = "Name must be at least 5 characters long")
   private String name;
 
   @NotEmpty(message = "You must choose at least one ingredient")
-  private List<String> ingredients;
+  private List<String> ingredients = Collections.emptyList();
 }
