dd4f354c84b62c4c656aed566c34f92ed3e9d338
Rafael Sousa Herves <rsherves@gmail.com>
2021-06-08 15:59:18 +0800

    Enhance Application Test by checking a bean exists in the Context

--- src/test/java/tacos/TacosApplicationTests.java
+++ src/test/java/tacos/TacosApplicationTests.java
@@ -1,13 +1,20 @@
 package tacos;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tacos.web.DesignTacoController;
 
 @SpringBootTest
 class TacosApplicationTests {
 
+	@Autowired
+	DesignTacoController tacoController; // any bean that should be present in the Application Context
+
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(tacoController);
 	}
 
 }
