aa73754aafba7a5776c9134c60fab6661ee7c707
Rafael Sousa Herves <rsherves@gmail.com>
2021-06-08 16:32:15 +0800

    Make Home Controller test start full Application Context:
     - Cannot use `@WebMvcTest(HomeController.class)`
       because HomeController class was deleted and added a
       View Controller instead.
    
     - Cannot use `@WebMvcTest` as it requires all beans in the context.
    
     - Replace `@WebMvcTest` with `@SpringBootTest` to start the full
       Spring Application Context.
    
     - Add `@AutoConfigureMockMvc` to be able to use `MockMvc` (not needed
       with annotation `@WebMvcTest`)

--- src/test/java/tacos/HomeViewControllerTest.java
+++ src/test/java/tacos/HomeViewControllerTest.java
@@ -1,18 +1,20 @@
 package tacos;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.web.servlet.MockMvc;
-
-@WebMvcTest
-public class HomeControllerTest {
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HomeViewControllerTest {
 
   @Autowired
   private MockMvc mockMvc;
